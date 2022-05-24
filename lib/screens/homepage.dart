32b24189b8f504425d168d502757e5cc7c475553
[alessandro rocco] <[alessandro.rocco97@gmail.com]>
2022-05-24 17:20:49 +0200

    [gian] push button bug solved

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,44 +1,35 @@
 import 'package:flutter/material.dart';
 
-
-class HomePage extends StatelessWidget {   
+class HomePage extends StatelessWidget {
   static const route = '/homepage/';
   static const routename = 'Homepage';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('HomePage')), 
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(   
-                child: Text('To ProfilePage'),            
-                onPressed: () {                         
-                  Navigator.pushNamed(context, 'profile');           
-                  
-                }
-              ),
-              ElevatedButton(  
-                child: Text('To CalendarPage'),             
-                onPressed: () {                          
-                  Navigator.pushNamed(context, 'calendar');          
-                  
-                }
-              ),
-
-              ElevatedButton(  
-                child: Text('To NotePage'),             
-                onPressed: () {                         
-                  Navigator.pushNamed(context, 'note');           
-                  
-                }
-              ),
-              
-            ],
-          ),
+      appBar: AppBar(title: Text('HomePage')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                child: Text('To ProfilePage'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/profilepage/');
+                }),
+            ElevatedButton(
+                child: Text('To CalendarPage'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/calendarpage/');
+                }),
+            ElevatedButton(
+                child: Text('To NotePage'),
+                onPressed: () {
+                  Navigator.pushNamed(context, '/notepage/');
+                }),
+          ],
         ),
+      ),
     );
   }
 }
