706347fba4e2a63c3f1bcd714a006a4788aa753f
[alessandro rocco] <[alessandro.rocco97@gmail.com]>
2022-06-01 22:03:29 +0200

    [master] login-homepage route and pop implemented with persistence

--- lib/screens/homepage.dart
+++ lib/screens/homepage.dart
@@ -1,13 +1,31 @@
+
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:colombo_rocco/screens/login.dart';
 
 class HomePage extends StatelessWidget {
   static const route = '/homepage/';
   static const routename = 'Homepage';
+  
+  void logout(BuildContext context) async {
+    final sp = await SharedPreferences.getInstance();
+    sp.remove('username');
+    Navigator.pop(context); 
+    Navigator.of(context).pushReplacementNamed('/login/');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('HomePage')),
+      appBar: AppBar(title: Text('HomePage'), actions: <Widget>[
+        IconButton(
+          icon: const Icon(Icons.logout),
+          tooltip: 'Logout',
+          onPressed: () {
+            logout(context);
+          },
+        ),
+      ]),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
